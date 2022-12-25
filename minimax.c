1fe94a632ed5cdbb6a095391af9b10d471f18756
pksh-23514 <prabhat23514k@gmail.com>
2022-12-25 11:24:40 +0530

    Minimax Algorithm implemented successfully

--- minimax.c
+++ minimax.c
@@ -1,23 +1,25 @@
 #include "tic_tac_toe.h"
-
-/* Minimax Algorithm Implementation */
-
+/*
+   To Implement the Minimax Algorithm for calculating the Best possible Move for the Computer in Single Player option.
+   Input - The Board Array, the Current Marker, the Number of Moves already played in the Game and which Player has to Mark on the Board on the Current Move.
+   Output - Returns the Maximum value for the Computer, Minimum value for the User and 0 if the Game is Over.
+ */
 int calculate_score (char board [SIZE][SIZE], char marker, int move_count, int move)
 {
 	int score = 0;
-	int best = 0;	//To store the Best Score.
+	int best = 0;	//To store the Best possible Score.
 
-	if ((game_over (board)) == SUCCESS)		//If the Game is Over, the Player having the Last Move has Won and similarly, the Score value shall be returned.
+	if ((game_over (board)) == SUCCESS)	//If the Game is Over, the Player having the Last Move has Won and similarly, the Score value shall be returned.
 	{
-		if (move == COMPUTER)	//If the Current move is of Computer, User has Won as the Last Move was played by User i.e. Worst Move.
+		if (move == COMPUTER)		//If the Current move is of Computer, User has Won as the Last Move was played by User i.e. Worst Move.
 			return -100;
 
-		if (move == USER)	//If the Current move is of User, Computer has Won as the Last Move was played by Computer i.e. Best Move.
+		if (move == USER)		//If the Current move is of User, Computer has Won as the Last Move was played by Computer i.e. Best Move.
 			return 100;
 	}
 	else	//The Game can be Over or not depending on the number of Moves played on the Board.
 	{
-		if (move_count < 9)		//The Game is not Over as Moves are still left and there are other possible scenarios to be checked for the Best Score calculation.
+		if (move_count < 9)	//The Game is not Over as Moves are still left and there are other possible scenarios to be checked for the Best Score calculation.
 		{
 			if (move == COMPUTER)	//Simulate the Moves taken by Computer till the Game is Over.
 			{
@@ -25,11 +27,15 @@ int calculate_score (char board [SIZE][SIZE], char marker, int move_count, int m
 
 				for (int i = 1; i <= (SIZE * SIZE); i++)
 				{
-					if (check_cell (board, i) == SUCCESS)	//If the Cell position is empty, it will be Marked by the Computer.
+					if (check_cell (board, i) == SUCCESS)		//If the Cell position is empty, it will be Marked by the Computer.
 					{
-						mark_board (board, marker, i);
-						marker = change_marker (marker);
+						mark_board (board, marker, i);		//To mark the 'i' with the 'marker' on the Board.
+						marker = change_marker (marker);	//To update the Marking for the next Move by the User.
+
+						/* Recursive Function call to simulate the Next Move taken by the User */
 						score = calculate_score (board, marker, (move_count + 1), USER);
+
+						/* This is a Back-Tracking Algorithm, so the Board shall be returned to the state it was before the Function call */
 						demark_board (board, i);
 						
 						if (score > best)	//If the Calculated Score is greater than the Best Score; update the Best Score.
@@ -45,11 +51,15 @@ int calculate_score (char board [SIZE][SIZE], char marker, int move_count, int m
 
 				for (int i = 1; i <= (SIZE * SIZE); i++)
 				{
-					if (check_cell (board, i) == SUCCESS)	//If the Cell position is empty, it will be Marked by the User.
+					if (check_cell (board, i) == SUCCESS)		//If the Cell position is empty, it will be Marked by the User.
 					{
-						mark_board (board, marker, i);
-						marker = change_marker (marker);
+						mark_board (board, marker, i);		//To mark the 'i' with the 'marker' on the Board.
+						marker = change_marker (marker);	//To update the Marking for the next Move by the Computer.
+
+						/* Recursive Function call to simulate the Next Move taken by the Computer */
 						score = calculate_score (board, marker, (move_count + 1), COMPUTER);
+
+						/* This is a Back-Tracking Algorithm, so the Board shall be returned to the state it was before the Function call */
 						demark_board (board, i);
 
 						if (score < best)	//If the Calculated Score is smaller than the Best Score; update the Best Score for User.
