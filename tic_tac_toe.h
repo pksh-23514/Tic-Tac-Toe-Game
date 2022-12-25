38d0f57ec12266219803988526032e0201d0b72b
pksh-23514 <prabhat23514k@gmail.com>
2022-12-25 11:56:37 +0530

    Header File implemented successfully

--- tic_tac_toe.h
+++ tic_tac_toe.h
@@ -1,30 +1,38 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard Libraries Inclusion */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <limits.h>
 
-#define SIZE 3
-#define FIRST 'O'
-#define SECOND 'X'
-#define COMPUTER 1
-#define USER 2
-#define SUCCESS 0
-#define FAILURE -1
+/* MACRO Definitions */
+#define SIZE 		3
+#define FIRST 		'O'
+#define SECOND 		'X'
+#define COMPUTER 	1
+#define USER 		2
+#define USER1 		1
+#define USER2 		2
+#define SUCCESS 	0
+#define FAILURE 	-1
 
 /* Function Declarations */
 
+/* Play the Tic-Tac-Toe Game in the selected Player Mode */
 void play_game ();
 
+/* Unit-Testing */
 void run_tests ();
 
+/* Single Player Mode */
 void play_game_single (int);
 int optimized_move (char [SIZE][SIZE], char, int);
 int calculate_score (char [SIZE][SIZE], char, int, int);
 
+/* Board Operations */
 void initialize_board (char [SIZE][SIZE]);
 void print_board (char [SIZE][SIZE]);
 int check_cell (char [SIZE][SIZE], int);
@@ -36,6 +44,7 @@ int row_win (char [SIZE][SIZE]);
 int col_win (char [SIZE][SIZE]);
 int diagonal_win (char [SIZE][SIZE]);
 
-void play_game_double ();
+/* Double Player Mode */
+void play_game_double (int);
 
 #endif
